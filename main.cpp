607a3524dabf0e98177ff56e0b0024436774eed8
Mishal <eng.mishal.ali@gmail.com>
2017-08-18 21:56:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "mbed.h"
 
-//------------------------------------
+//-----------------------------------------------
 // This code for Maxbotix MB1030 LV-MaxSonar-EZ3
 // Read distance Values in Meters
 // Target Board: NUCLEO-L476RG
-// Auther: Mishal Hamed   Alm3shaws@hotmail.com
-//------------------------------------
+// Vin -> 3.3V
+// GND -> GND
+// AN -> A0 (PA_0)
+// Auther: Mishal Hamed
+//----------------------------------------------
 
 Serial pc(SERIAL_TX, SERIAL_RX);
 AnalogIn Distance(PA_0);
@@ -13,13 +16,15 @@ AnalogIn Distance(PA_0);
 int main()
 {
     float measure;
-
+    
+//Show results using serial monitor
     pc.printf("started! !\n");
     
+//Reading the results    
     while(1) {
             measure = (((Distance.read()*(3.3/4096))/2)*10000);
             measure = (measure/0.0064)*0.0254;
             pc.printf("Distance = %.2f Meter \n", measure);
-            wait(2);
+            wait(2); //Delay
     }
 }
